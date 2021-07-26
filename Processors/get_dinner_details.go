8b885465c493163169f1a9f94f83bfefe68624ff
aaronangxz <angxze@gmail.com>
2021-07-27 01:04:32 +0800

    Started get_menu

---
+++ Processors/get_dinner_details.go
@@ -0,0 +1,29 @@
+package Processors
+
+import (
+	"fmt"
+
+	"github.com/aaronangxz/SeaDinner/AuthToken"
+	"github.com/go-resty/resty/v2"
+)
+
+func GetCurrent(client resty.Client) {
+	var currentmenu Current
+
+	_, err := client.R().
+		SetHeader("Authorization", AuthToken.GetToken()).
+		SetResult(&currentmenu).
+		Get("https://dinner.sea.com/api/current")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Query status of today's menu: %v\n\n", currentmenu.Status)
+	fmt.Printf("ID: %v\n", currentmenu.Details.Id)
+	fmt.Printf("%v\n", currentmenu.Details.Name)
+	//fmt.Printf("%v\n", currentmenu.Details.Comment)
+	fmt.Printf("Start: %v\n", currentmenu.Details.PollStart)
+	fmt.Printf("End: %v\n", currentmenu.Details.PollEnd)
+	fmt.Printf("Serving Time: %v\n", currentmenu.Details.ServingTime)
+}
--- main.go
+++ main.go
@@ -1,69 +1,18 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/aaronangxz/SeaDinner/AuthToken"
+	"github.com/aaronangxz/SeaDinner/Processors"
 	"github.com/go-resty/resty/v2"
 )
-
-type DinnerMenu struct {
-	status string
-	dishes Food
-}
-
-type Current struct {
-	Status  string `json:"status"`
-	Details Menu   `json:"menu"`
-}
-
-type Food struct {
-	code        string
-	id          string
-	name        string
-	description string
-	image       string
-	ordered     int
-	quota       int
-	disabled    bool
-}
-
-type Menu struct {
-	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Comment     string `json:"comment"`
-	PollStart   string `json:"pollstart"`
-	PollEnd     string `json:"pollend"`
-	ServingTime string `json:"servingtime"`
-	Active      bool   `json:"active"`
-}
 
 func main() {
 	// Create a Resty Client
 	client := resty.New()
 
 	//get today's dinner info
-	GetCurrent(*client)
+	// Processors.GetCurrent(*client)
 
-}
+	//get today's menu
+	Processors.GetMenu(*client)
 
-func GetCurrent(client resty.Client) {
-	var currentmenu Current
-
-	_, err := client.R().
-		SetHeader("Authorization", AuthToken.GetToken()).
-		SetResult(&currentmenu).
-		Get("https://dinner.sea.com/api/current")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Printf("Query status of today's menu: %v\n\n", currentmenu.Status)
-	fmt.Printf("ID: %v\n", currentmenu.Details.Id)
-	fmt.Printf("%v\n", currentmenu.Details.Name)
-	//fmt.Printf("%v\n", currentmenu.Details.Comment)
-	fmt.Printf("Start: %v\n", currentmenu.Details.PollStart)
-	fmt.Printf("End: %v\n", currentmenu.Details.PollEnd)
-	fmt.Printf("Serving Time: %v\n", currentmenu.Details.ServingTime)
 }
