67c9113f81dd2043eb676736b759b67b6f9ca05b
aaronangxz <angxze@gmail.com>
2022-05-06 14:56:45 +0800

    Changed post body to form data

--- main.go
+++ main.go
@@ -20,10 +20,11 @@ func main() {
 	Processors.GetMenu(*client, ID)
 	lunchTime := Processors.GetLunchTime()
 	var req Processors.OrderRequest
-	req.FoodID = "1141"
-	fmt.Println(time.Now().Unix())
-	fmt.Println(lunchTime.Unix())
+	req.FoodID = 1194
 
+	fmt.Println("Waiting..")
+
+	// Processors.OrderDinner(*client, ID, req)
 	for {
 		if time.Now().Unix() == lunchTime.Unix() {
 			for i := 0; i < 1; i++ {
@@ -31,13 +32,5 @@ func main() {
 				Processors.OrderDinner(*client, ID, req)
 			}
 		}
-
-		// for i := 0; i < 30; i++ {
-		// 	fmt.Println("Attempt ", i)
-		// 	Processors.OrderDinner(*client, ID, req)
-		// }
 	}
-
-	//order using todays ID
-	//Processors.OrderDinnerQuery(*client, ID)
 }
