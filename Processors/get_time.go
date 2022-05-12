1ff317f5a0c51833da8fdedd7d53924acb5f8bd0
aaronangxz <angxze@gmail.com>
2022-05-12 14:47:27 +0800

    Added order time in config

--- Processors/get_time.go
+++ Processors/get_time.go
@@ -17,5 +17,5 @@ func GetLunchTime() time.Time {
 	now := time.Now().In(tz)
 
 	year, month, day := now.Date()
-	return time.Date(year, month, day, 12, 30, 0, 0, now.Location())
+	return time.Date(year, month, day, Config.OrderTime.Hour, Config.OrderTime.Minutes, Config.OrderTime.Seconds, 0, now.Location())
 }
