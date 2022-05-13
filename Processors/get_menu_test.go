37a254cf6e34be4516f3345a38063efaa8187aec
aaronangxz <angxze@gmail.com>
2022-05-14 01:36:09 +0800

    Removed API call tests'

--- Processors/get_menu_test.go
+++ Processors/get_menu_test.go
@@ -1,38 +1,30 @@
 package Processors
 
-import (
-	"os"
-	"reflect"
-	"testing"
+// func TestGetMenu(t *testing.T) {
+// 	LoadEnv()
+// 	r := Init()
 
-	"github.com/go-resty/resty/v2"
-)
-
-func TestGetMenu(t *testing.T) {
-	LoadEnv()
-	r := Init()
-
-	type args struct {
-		client resty.Client
-		ID     int
-		key    string
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{
-			name: "HappyCase",
-			args: args{client: r, ID: 3521, key: os.Getenv("TOKEN")},
-			want: 8,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetMenu(tt.args.client, tt.args.ID, tt.args.key); !reflect.DeepEqual(len(got.DinnerArr), tt.want) {
-				t.Errorf("GetMenu() = %v, want %v", len(got.DinnerArr), tt.want)
-			}
-		})
-	}
-}
+// 	type args struct {
+// 		client resty.Client
+// 		ID     int
+// 		key    string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want int
+// 	}{
+// 		{
+// 			name: "HappyCase",
+// 			args: args{client: r, ID: 3521, key: os.Getenv("TOKEN")},
+// 			want: 8,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := GetMenu(tt.args.client, tt.args.ID, tt.args.key); !reflect.DeepEqual(len(got.DinnerArr), tt.want) {
+// 				t.Errorf("GetMenu() = %v, want %v", len(got.DinnerArr), tt.want)
+// 			}
+// 		})
+// 	}
+// }
