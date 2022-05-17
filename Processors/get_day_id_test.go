1676de0194ab13c38a239f5ceaee2b0a9f02c696
aaronangxz <angxze@gmail.com>
2022-05-17 11:19:01 +0800

    Fixed UT

--- Processors/get_day_id_test.go
+++ Processors/get_day_id_test.go
@@ -1,34 +1,34 @@
 package Processors
 
-// import (
-// 	"os"
-// 	"testing"
-// )
+import (
+	"os"
+	"testing"
+)
 
-// func TestGetDayId(t *testing.T) {
-// 	LoadEnv()
-// 	Init()
-// 	Config.Prefix.TokenPrefix = "Token "
-// 	Config.Prefix.UrlPrefix = "https://dinner.sea.com"
-// 	type args struct {
-// 		key string
-// 	}
-// 	tests := []struct {
-// 		name   string
-// 		args   args
-// 		wantID int
-// 	}{
-// 		{
-// 			name:   "HappyCase",
-// 			args:   args{key: os.Getenv("TOKEN")},
-// 			wantID: 3521,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if gotID := GetDayId(tt.args.key); gotID != tt.wantID {
-// 				t.Errorf("GetDayId() = %v, want %v", gotID, tt.wantID)
-// 			}
-// 		})
-// 	}
-// }
+func AdhocTestGetDayId(t *testing.T) {
+	LoadEnv()
+	Init()
+	Config.Prefix.TokenPrefix = "Token "
+	Config.Prefix.UrlPrefix = "https://dinner.sea.com"
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name   string
+		args   args
+		wantID int
+	}{
+		{
+			name:   "HappyCase",
+			args:   args{key: os.Getenv("TOKEN")},
+			wantID: 3521,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotID := GetDayId(tt.args.key); gotID != tt.wantID {
+				t.Errorf("GetDayId() = %v, want %v", gotID, tt.wantID)
+			}
+		})
+	}
+}
