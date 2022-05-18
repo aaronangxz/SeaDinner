45cdf4325615ed6ae21ff830f3d84dbd2ff27f16
aaronangxz <angxze@gmail.com>
2022-05-18 12:13:26 +0800

    Added filter while prepping order

--- Processors/prepare_order.go
+++ Processors/prepare_order.go
@@ -3,14 +3,26 @@ package Processors
 import (
 	"fmt"
 	"log"
+	"strings"
 )
 
 func PrepOrder() ([]UserChoiceWithKeyAndStatus, bool) {
 	var (
 		record []UserChoiceWithKeyAndStatus
 	)
+
+	m := MakeMenuMap()
+	inQuery := "("
+	for e := range m {
+		inQuery += e + ", "
+	}
+	inQuery += ")"
+	inQuery = strings.ReplaceAll(inQuery, ", )", ")")
+	query := fmt.Sprintf("SELECT c.*, k.user_key FROM user_choice_tab c, user_key_tab k WHERE user_choice IN %v AND c.user_id = k.user_id", inQuery)
+	log.Println(query)
+
 	//check whole db
-	if err := DB.Raw("SELECT c.*, k.user_key FROM user_choice_tab c, user_key_tab k WHERE c.user_id = k.user_id").Scan(&record).Error; err != nil {
+	if err := DB.Raw(query).Scan(&record).Error; err != nil {
 		fmt.Println(err.Error())
 		return nil, false
 	}
