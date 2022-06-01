ec67a3b4a51c1ae857b8f13324b71cc20217f149
aaronangxz <angxze@gmail.com>
2022-06-01 11:44:15 +0800

    Deprecated /chope command

--- Client/main.go
+++ Client/main.go
@@ -134,13 +134,16 @@ func main() {
 				msg.ParseMode = "HTML"
 			}
 		case "chope":
-			s, ok := Bot.CheckKey(update.Message.Chat.ID)
-			if !ok {
-				msg.Text = s
-			} else {
-				msg.Text = "What do you want to order? \nTell me the Food ID 😋 \nEnter -1 to cancel dinner ordering 🙅"
-				startListenChope = true
-			}
+			msg.Text = "This command is deprecated. Choose from /menu instead!😋"
+			/*
+				s, ok := Bot.CheckKey(update.Message.Chat.ID)
+				if !ok {
+					msg.Text = s
+				} else {
+					msg.Text = "What do you want to order? \nTell me the Food ID 😋 \nEnter -1 to cancel dinner ordering 🙅"
+					startListenChope = true
+				}
+			*/
 		case "choice":
 			s, ok := Bot.CheckKey(update.Message.Chat.ID)
 			if !ok {
