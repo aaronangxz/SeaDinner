983441966b5469357332fe71b06135779771c68d
aaronangxz <angxze@gmail.com>
2022-06-07 22:13:05 +0800

    Update codecov

--- Client/main.go
+++ Client/main.go
@@ -32,7 +32,7 @@ func main() {
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	u.Timeout = 3600
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
